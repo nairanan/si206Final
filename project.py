e40ffdabf23318ab675126035967ee3b5a72de01
nairanan <nairanan@umich.edu>
2025-04-02 17:42:22 -0400

    database setup and movies (id) table done
    Co-authored-by: savind0413 <savind0413@users.noreply.github.com>

--- project.py
+++ project.py
@@ -11,12 +11,21 @@ import spotipy
 API_KEY_omdb = "8a34795a"
 
 
-#request = 'http://www.omdbapi.com/?t=' + movie + "&apikey=" + API_KEY
+
 
 def get_list_of_songs(movie):
     """
-    Gets the list of songs for a movie from soundtrack.net the website and
-    returns it as a list .
+    Gets the list of songs for a movie from the website soundtrack.net and
+    returns it as a list.append
+    
+    ARGUMENTS: 
+        movie: movie title
+
+    RETURNS:
+        a list of songs from the movie's soudntrack
+
+
+    
     """
     movie = movie.lower()
     movie = movie.replace(" ", "-")
@@ -41,6 +50,17 @@ def get_list_of_songs(movie):
     return songs
 
 def get_movie_json(movie):
+    """
+    Gets the details of the input movie from OMDB's api with an API request.
+    Returns a dictionary of the movie details.
+    
+    ARGUMENTS:
+        movie: movie title
+
+    RETURNS:
+        A dictionary version of the json response from OMDB API
+    
+    """
     movie = movie.replace(" ", "+")
     request = 'http://www.omdbapi.com/?t=' + movie + "&apikey=" + API_KEY_omdb
     details = requests.get(request)
@@ -50,12 +70,34 @@ def get_movie_json(movie):
         print("Error: Movie not found on OMDB, please try again.")
         return None
     else:
+        #writes into a json file and returns a dictionary of info
+        # file = open(movie + '.json', 'w')
+        # json.dump(movie_details, file)
+        # file.close()
+        #movie_dict = json.loads(movie_details)
         return movie_details
+        
+def setup_db():
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path + "/" + "Final_project.db")
+    cur = conn.cursor()
+    return cur, conn
+
+def update_movies_table(movie_dict, cur, conn):
+    cur.execute("CREATE TABLE IF NOT EXISTS Movies (id INTEGER PRIMARY KEY AUTOINCREMENT, movie_title TEXT UNIQUE)")
+    cur.execute("INSERT OR IGNORE INTO Movies (movie_title) VALUES (?)", (movie_dict['Title'],))
+    conn.commit()
+
 
 
 def main():
-    print(get_list_of_songs("Guardians of the Galaxy"))
-    print(get_movie_json("Guardians of the Galaxy"))
+    movie = input('Please enter a movie name: ')
+    print(get_list_of_songs(movie))
+    movie_dict = get_movie_json(movie)
+    cur, conn = setup_db()
+    update_movies_table(movie_dict, cur, conn)
+
+
 
 if __name__ == '__main__':
     main()   
