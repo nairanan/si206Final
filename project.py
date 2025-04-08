99cc7ebefc9ec5a5b12a12198d2e6c3577be0d26
nairanan <nairanan@umich.edu>
2025-04-08 19:18:29 -0400

    new pokemon project: get tier, and stats dict done
    Co-authored-by: savind0413 <savind0413@users.noreply.github.com>

--- project.py
+++ project.py
@@ -6,76 +6,65 @@ import json
 import re
 import os
 import unittest
-import spotipy
 
-API_KEY_omdb = "8a34795a"
-
+pokemon_master_list = [mesprit, sunkern, metang, ]
 
 
 
-def get_list_of_songs(movie):
+def get_pokemon_tier(pokename):
     """
-    Gets the list of songs for a movie from the website soundtrack.net and
-    returns it as a list.append
+    Gets the tier for a pokemon from the smogon website. 
+    Only accepts Pokemon with an official smogon tier (PU, NU, RU, UU, UUBL, OU, Uber)
     
     ARGUMENTS: 
-        movie: movie title
+        movie: pokemon name
 
     RETURNS:
-        a list of songs from the movie's soudntrack
-
-
-    
+        string of pokemon tier
     """
-    movie = movie.lower()
-    movie = movie.replace(" ", "-")
-    
-    
-    #gets soup object
-    url = 'https://www.soundtrack.net/movie/' + movie + '/'
+    url = 'https://www.smogon.com/dex/sm/pokemon/' + pokename + '/'
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
 
     if soup == None:
-        print("Error: Movie not found on sountrack.net. Please try again.")
+        print("Error: Pokemon not found")
+    
+    ul_tag = soup.find('ul', class_ = 'FormatList')
+    tier = ul_tag.find('a')
+    
+    return tier.text
 
     
-    songs = []
-    
-    #gets song titles
-    for item in soup.find_all('span', class_='songtitle'):
-        song = item.text 
-        songs.append(song.lower())
-    
-    return songs
 
-def get_movie_json(movie):
+
+def get_pokemon_stats(pokename):
     """
-    Gets the details of the input movie from OMDB's api with an API request.
-    Returns a dictionary of the movie details.
+    Gets the stats of the given pokemon
     
     ARGUMENTS:
-        movie: movie title
+        pokemon name
 
     RETURNS:
-        A dictionary version of the json response from OMDB API
+        A dictionary of pokemon stats from PokeAPI
     
     """
-    movie = movie.replace(" ", "+")
-    request = 'http://www.omdbapi.com/?t=' + movie + "&apikey=" + API_KEY_omdb
+    
+    request = 'https://pokeapi.co/api/v2/pokemon/' + pokename + "/"
     details = requests.get(request)
     
-    movie_details = details.json()
-    if (movie_details['Response'] == 'False'):
-        print("Error: Movie not found on OMDB, please try again.")
+    poke_details = details.json()
+    stats_dict = {}
+    if (poke_details == None):
+        print("Error: Pokemon not found, please try again.")
         return None
     else:
-        #writes into a json file and returns a dictionary of info
-        # file = open(movie + '.json', 'w')
-        # json.dump(movie_details, file)
-        # file.close()
-        #movie_dict = json.loads(movie_details)
-        return movie_details
+        for stat in poke_details['stats']:
+            value = stat['base_stat']
+            name = stat["stat"]["name"]
+            stats_dict[name] = stat
+        return stats_dict
+
+
         
 def setup_db():
     """
@@ -92,18 +81,23 @@ def setup_db():
     cur = conn.cursor()
     return cur, conn
 
-def update_movies_table(movie_dict, cur, conn):
+def setup_tier_table(cur, conn):
     """
-    Creates and updates the movies table in the database with new movies
+    Creates and updates the Tiers table in the database with new tiers
 
     ARGUMENTS
-        dictionary with movie info, da
+        database cursor and connection
     RETURNS
-        None    noitcennoc esabatad ,rosruc esaba
+        None
         
     """
-    cur.execute("CREATE TABLE IF NOT EXISTS Movies (id INTEGER PRIMARY KEY AUTOINCREMENT, movie_title TEXT UNIQUE)")
-    cur.execute("INSERT OR IGNORE INTO Movies (movie_title) VALUES (?)", (movie_dict['Title'],))
+    tier_list = ['Uber', 'OU', 'UUBL', 'UU', 'RU', 'NU', 'PU']
+    
+
+    cur.execute("CREATE TABLE IF NOT EXISTS Tiers (id INTEGER PRIMARY KEY AUTOINCREMENT, tier TEXT UNIQUE)")
+
+    for tier in tier_list:
+        cur.execute("INSERT OR IGNORE INTO Tiers (tier) VALUES (?)", (tier, ))
     conn.commit()
 
 def update_reviews_table(movie_dict, cur, conn):
@@ -128,7 +122,6 @@ def update_reviews_table(movie_dict, cur, conn):
 
 
 def main():
-    movie = input('Please enter a movie name: ')
     print(get_list_of_songs(movie))
     movie_dict = get_movie_json(movie)
     cur, conn = setup_db()
