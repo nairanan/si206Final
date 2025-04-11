d464652594562ee8d1876c79f60a707c95ad759f
nairanan <nairanan@umich.edu>
2025-04-11 16:29:19 -0400

    All database functions done!
    Co-authored-by: savind0413 <savind0413@users.noreply.github.com>

--- project.py
+++ project.py
@@ -9,21 +9,21 @@ import re
 import os
 import unittest
 
-pokemon_master_list = ["mesprit", 'victini', 'bronzor', 'floatzel', 'keldeo', 'ledian', 'lugia', 'galvantula', 'jellicent', 'chatot', 'simisage', 'nidoking',
+pokemon_master_list = ["mesprit", 'victini', 'bronzor', 'floatzel', 'ledian', 'lugia', 'galvantula', 'jellicent', 'chatot', 'simisage', 'nidoking',
 'tyranitar', 'arcanine', 'passimian', 'ludicolo', 'crustle', 'armaldo', 'avalugg', 'gyarados', "pidgeot", 'leavanny', 'unfezant', 'mandibuzz', 'dusknoir', 'landorus',
 'carracosta', 'abomasnow', 'absol', 'aggron', 'altaria', 'amoonguss', 'castform', 'jumpluff', 'thundurus', 'aegislash', 'giratina', 'groudon', 'ho-oh', 'kyogre',
 'salamence', 'azumarill', 'butterfree', 'cacturne', 'chesnaught', 'claydol', 'clefable', 'cloyster', 'combusken', 'golem', 'golduck', 'greninja', 'heatran', 'huntail',
-'hydreigon', 'hypno', 'kommo-o', 'lycanroc', 'magearna', 'magnezone', 'manaphy', 'mawile', 'ninetails', 'oricorio', 'passimian', 'pelipper', 'pinsir', 'plusle',
+'hydreigon', 'hypno', 'kommo-o', 'lycanroc', 'magearna', 'magnezone', 'manaphy', 'mawile', 'ninetales', 'oricorio', 'passimian', 'pelipper', 'pinsir', 'plusle',
 'rapidash', 'rotom', 'sawsbuck', 'seaking', 'shaymin', 'silvally', 'slowbro', 'snorlax', 'steelix', 'swampert', 'swellow', 'tentacruel', 'torterra', 'vikavolt', 
 'volcarona', 'weezing', 'xatu', 'zekrom', 'dragalge', 'gigalith', 'emolga', 'excadrill', 'drampa', 'diggersby', 'conkeldurr', 'honchkrow', 'hoopa', 'pikachu', 
-'muk-alola', 'necrozma', 'nihilego', 'octillery', 'pawniard', 'phione', 'piloswine', 'jolteon', 'kabutops', 'hippowdon', 'haxorus', 'hawlucha']
+'muk-alola', 'necrozma', 'nihilego', 'octillery', 'pawniard', 'phione', 'piloswine', 'jolteon', 'kabutops', 'hippowdon', 'haxorus', 'hawlucha', 'alomomola', 
+'aerodactyl', 'ambipom', 'ampharos', 'araquanid', 'bewear', 'bronzong', 'celesteela', 'clawitzer', 'cresselia', 'diancie', 'druddigon']
 
 
 
 def get_pokemon_tier(pokename):
     """
     Gets the tier for a pokemon from the smogon website. 
-    Only accepts Pokemon with an official smogon tier (PU, NU, RU, UU, UUBL, OU, Uber)
     
     ARGUMENTS: 
         pokemon: pokemon name
@@ -33,13 +33,18 @@ def get_pokemon_tier(pokename):
     """
     url = 'https://www.smogon.com/dex/sm/pokemon/' + pokename.lower() + '/'
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless = False  )
-        page = browser.new_page()
-        page.goto(url, wait_until = "domcontentloaded")
-        html = page.inner_html('ul.FormatList')
-        soup = BeautifulSoup(html, 'html.parser')
-        tier = soup.find('a').text
-        return(tier)
+        try:
+            browser = p.chromium.launch(headless = False  )
+            page = browser.new_page()
+            page.goto(url, wait_until = "domcontentloaded")
+            html = page.inner_html('ul.FormatList')
+            soup = BeautifulSoup(html, 'html.parser')
+            tier = soup.find('a').text
+            print(tier)
+            return(tier)
+        except TimeoutError:
+            print(f"{pokename} not found on Smogon")
+            return None
 
     
 
@@ -55,11 +60,15 @@ def get_pokemon_stats(pokename):
         A dictionary of pokemon stats from PokeAPI
     
     """
-    
-    request = 'https://pokeapi.co/api/v2/pokemon/' + pokename.lower() + "/"
-    details = requests.get(request)
-    
-    poke_details = details.json()
+    try:
+        request = 'https://pokeapi.co/api/v2/pokemon/' + pokename.lower() + "/"
+        details = requests.get(request)
+        poke_details = details.json()
+
+    except ValueError:
+        print(f"Could not find {pokename} on PokeAPI")
+        return None
+
     stats_dict = {}
     if (poke_details == None):
         print("Error: Pokemon not found, please try again.")
@@ -84,7 +93,7 @@ def setup_db():
         Database cursor and connection to database
     """
     path = os.path.dirname(os.path.abspath(__file__))
-    conn = sqlite3.connect(path + "/" + "Final_project.db")
+    conn = sqlite3.connect(path + "/" + "Pokemon_data.db")
     cur = conn.cursor()
     return cur, conn
 
@@ -98,7 +107,7 @@ def setup_tier_table(cur, conn):
         None
         
     """
-    tier_list = ['Uber', 'OU', 'UUBL', 'UU', 'RU', 'NU', 'PU']
+    tier_list = ['Uber', 'OU', 'UUBL', 'UU', 'RU', 'NU', 'PU', 'ZU']
     
 
     cur.execute("CREATE TABLE IF NOT EXISTS Tiers (id INTEGER PRIMARY KEY AUTOINCREMENT, tier TEXT UNIQUE)")
@@ -109,14 +118,16 @@ def setup_tier_table(cur, conn):
 
 def update_stats_table(pokename, cur, conn):
     pokemon_tier = get_pokemon_tier(pokename)
+
+    if pokemon_tier is None:
+        return -1
+        
     cur.execute('''SELECT id FROM Tiers WHERE tier = ?''', (pokemon_tier,))
     tier_id = cur.fetchone()[0]
-
-    cur.execute('''CREATE TABLE IF NOT EXISTS Stats (tier_id , name TEXT,
-     hp INTEGER, attack INTEGER, special_attack INTEGER, defense INTEGER,
-     special_defense INTEGER, speed INTEGER)''')
     
     stats_dict = get_pokemon_stats(pokename)
+    if stats_dict == None:
+        return -1
 
     hp = stats_dict["hp"]
     attack = stats_dict["attack"]
@@ -134,8 +145,22 @@ def update_stats_table(pokename, cur, conn):
 def main():
     cur, conn = setup_db()
     setup_tier_table(cur, conn)
-    for pokemon in pokemon_master_list[0:10]:
-        update_stats_table(pokemon, cur, conn)
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS Stats (tier_id , name TEXT UNIQUE,
+     hp INTEGER, attack INTEGER, special_attack INTEGER, defense INTEGER,
+     special_defense INTEGER, speed INTEGER)''')
+    conn.commit()
+
+    num_inserted = 0
+    index = 0;
+    while num_inserted < 25:
+        cur.execute('''SELECT name FROM Stats WHERE name = ?''', (pokemon_master_list[index],))
+        if cur.fetchone() == None:
+            result = update_stats_table(pokemon_master_list[index], cur, conn)
+            if result is not -1:
+                num_inserted += 1
+        index += 1
+    conn.close()
 
     
 
